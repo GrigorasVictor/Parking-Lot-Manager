8b0fe46eeee76eef2f63b6d9de50540c925cb67a
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-07-26 16:29:00 +0300

    update: std

--- PythonAI/AI/scripts/yoloPredictor.py
+++ PythonAI/AI/scripts/yoloPredictor.py
@@ -1,14 +1,18 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8x.pt")  # pretrained YOLOv8n model
-print(model.names)
+model = YOLO("yolov8n.pt")  # pretrained YOLOv8n model
+# print(model.names)
 
 # Run batched inference on a list of images
 # results = model("bus.jpg")  # return a list of Results objects
 
-# results = model.predict("bus.jpg", conf=0.7, classes = 0)
-# results[0].show()
+result = model.predict("bus.png", conf=0.7)[0]
+result.show()
+
+for box in result.boxes:
+    if(box.cls[0] == 0): print("la " + str(box.xyxy) + " e om")
+    else: print("la " + str(box.xyxy) + "  nu  e om")
 
 
 
