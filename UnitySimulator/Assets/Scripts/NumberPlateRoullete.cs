fac9cfa379e53716b398c1ed6ee500b30de2092e
GrigorasVictor <victorandrei201112@gmail.com>
2024-08-02 16:21:52 +0300

    modify unitySim

--- UnitySimulator/Assets/Scripts/NumberPlateRoullete.cs
+++ UnitySimulator/Assets/Scripts/NumberPlateRoullete.cs
@@ -9,17 +9,12 @@ public class NumberPlateRoullete : MonoBehaviour
     static string[] numberPlates = { "AB 10 FFV", "AG 49 XRF", "AR 05 DAN", "BC 10 VIC", "BH 19 ERP", "BN 23 GOA", "BR 50 ETT", "BT 41 XYZ", 
                                      "BV 91 ABC", "CJ 69 CLJ", "CL 33 DFE", "CS 73 SAD","AG 42 BXF", "MH 27 RQK", "CS 58 WZN"};
     private string numberPlate = string.Empty;
-    public static bool[] isTaken = new bool[numberPlates.Length];
-    public static int index = 99; 
+    //public static bool[] isTaken = new bool[numberPlates.Length];
+    public static int index; 
     // Start is called before the first frame update
     void Start()
     {
-        index = UnityEngine.Random.Range(0, numberPlates.Length);
-        while(isTaken[index] == true)
-        {
-            index = UnityEngine.Random.Range(0, numberPlates.Length);
-        }
-        isTaken[index] = true;
+        index = UnityEngine.Random.Range(0, numberPlates.Length);        
         numberPlate = numberPlates[index];         
         frontPlate.text = numberPlate;
         backPlate.text = numberPlate;
--- UnitySimulator/Assets/Scripts/PathFinder.cs
+++ UnitySimulator/Assets/Scripts/PathFinder.cs
@@ -13,8 +13,6 @@ public class PathFinder : MonoBehaviour
 
     public int isAllowedToEnter = 1; // 0 - nu; 1 - inca nu se stie; 2 - da
     public int state = 0; // 0 - entering; 1 - stationed; 2 - exiting
-
-    public int index = NumberPlateRoullete.index;
     public CarGenerator carGenerator;
 
     void Start()
@@ -66,7 +64,6 @@ public class PathFinder : MonoBehaviour
     {
         if (Input.GetKeyDown(("" + (id + 1))))
         {
-            Debug.Log("apasat");
             state = 2;
             start = this.transform.position;
             final = positions[futurePosition % 3].position;
@@ -112,7 +109,6 @@ public class PathFinder : MonoBehaviour
             else
             {
                 carGenerator.parked[id] = false;
-                NumberPlateRoullete.isTaken[index] = false;
                 Destroy(this.gameObject);
             }
         }
