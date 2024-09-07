322767dc28424816b33a68e243f8abab26eebffa
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-09-07 15:19:48 +0300

    merged with photo upload

---
+++ back-end/src/main/java/admin/parkWise/administration/controller/VehicleRegistrationController.java
@@ -0,0 +1,11 @@
+package admin.parkWise.administration.controller;
+import admin.parkWise.administration.model.VehicleRegistration;
+import admin.parkWise.administration.repository.VehicleRegistrationRepo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicleRegistration")
+public class VehicleRegistrationController extends AbstractController<VehicleRegistration, VehicleRegistrationRepo>{
+}
