47c5098375947dce4325f856d7ead60fdd95f99c
GrigorasVictor <victorandrei201112@gmail.com>
2024-09-13 00:55:25 +0300

    added auth madafacar

---
+++ back-end/src/main/java/admin/parkWise/administration/controller/UserAuthController.java
@@ -0,0 +1,31 @@
+package admin.parkWise.administration.controller;
+
+import admin.parkWise.administration.dto.LoginDto;
+import admin.parkWise.administration.model.UserAuth;
+import admin.parkWise.administration.repository.UserAuthRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserAuthController {
+
+    @Autowired
+    UserAuthRepo userAuthRepo;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> logIn(@RequestBody LoginDto newEntry) {
+        System.out.println(newEntry);
+
+        return new ResponseEntity<>("Login Successfully",HttpStatus.OK);
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<UserAuth> signIn(@RequestBody UserAuth newEntry) {
+        System.out.println(newEntry);
+
+        return new ResponseEntity<>(newEntry, HttpStatus.OK);
+    }
+}
---
+++ back-end/src/main/java/admin/parkWise/administration/dto/LoginDto.java
@@ -0,0 +1,14 @@
+package admin.parkWise.administration.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Setter
+@Getter
+@ToString
+public class LoginDto {
+    private String email;
+    private String password;
+
+
+}
