41c94c4b9dca4d66e0ba603f3e576e4b2be0515f
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-09-12 23:53:53 +0300

    updated user auth

--- back-end/src/main/java/admin/parkWise/administration/model/UserAuth.java
+++ back-end/src/main/java/admin/parkWise/administration/model/UserAuth.java
@@ -1,4 +1,65 @@
 package admin.parkWise.administration.model;
 
-public class UserAuth {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "user_auth")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAuth
+//        implements UserDetails
+        {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    @Getter
+    @Setter
+    private Integer userId;
+
+    @Column(name = "email", nullable = false)
+    @JsonProperty("email")
+    @Getter @Setter
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @JsonProperty("password")
+    @Getter @Setter
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of();
+//    }
+
+//    @Override
+    public String getUsername() {
+        return email;
+    }
+
+//    @Override
+    public boolean isEnabled() {return true;}
+//    @Override
+    public boolean isAccountNonExpired() {return true;}
+//    @Override
+    public boolean isAccountNonLocked() {return true;}
+//    @Override
+    public boolean isCredentialsNonExpired() {return true;}
 }
