4e950909503291191e5470d01ad38998e4282017
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-09-12 23:17:35 +0300

    make sure commit

---
+++ back-end/src/main/java/admin/parkWise/administration/model/UserAuth.java
@@ -0,0 +1,4 @@
+package admin.parkWise.administration.model;
+
+public class UserAuth {
+}
---
+++ back-end/src/main/java/admin/parkWise/administration/repository/UserAuthRepo.java
@@ -0,0 +1,10 @@
+package admin.parkWise.administration.repository;
+
+import admin.parkWise.administration.model.User;
+import admin.parkWise.administration.model.UserAuth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserAuthRepo extends JpaRepository<UserAuth, Integer> {
+}
