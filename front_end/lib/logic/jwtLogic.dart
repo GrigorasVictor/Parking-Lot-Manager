9916cabf164d139a2cbad8d0be5ec5993bdaccc8
GrigorasVictor <victorandrei201112@gmail.com>
2024-10-01 10:15:00 +0300

    fixed a bug at parser

--- front_end/lib/logic/jwtLogic.dart
+++ front_end/lib/logic/jwtLogic.dart
@@ -3,7 +3,7 @@ String? getJwtCookie(Iterable<String> cookies) {
   String? curCookie;
   
   while (it.moveNext()) {
-    RegExp regExp = RegExp(r"(?<=jwToken=).*"); //match after the '='
+    RegExp regExp = RegExp(r"(?<=jwToken=)[^ ]*"); //match after the '=' and stop at the first ' '
     Iterable<RegExpMatch> parsedData = regExp.allMatches(it.current.toString());
     curCookie = parsedData.isNotEmpty ? parsedData.first.group(0) : null;
     if (curCookie != null) {
