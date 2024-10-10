7ab019c70206959729736623a732d2fa6852b6f3
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-10-10 11:39:35 +0300

    chore: fixed subscription templates to db

--- back-end/src/main/java/admin/parkWise/administration/controllers/SubscriptionTemplateController.java
+++ back-end/src/main/java/admin/parkWise/administration/controllers/SubscriptionTemplateController.java
@@ -2,7 +2,11 @@ package admin.parkWise.administration.controllers;
 
 import admin.parkWise.administration.models.SubscriptionTemplate;
 import admin.parkWise.administration.repository.SubscriptionTemplateRepo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/subscription-templates")
 public class SubscriptionTemplateController extends AbstractController<SubscriptionTemplate, SubscriptionTemplateRepo> {
 
 }
