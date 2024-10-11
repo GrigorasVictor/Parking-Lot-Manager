5e190699f3e762e158eeed186f59c4cf5bb7f34d
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-10-11 17:25:09 +0300

    chore: updated allowed end-points

--- back-end/src/main/java/admin/parkWise/administration/configs/SecurityConfig.java
+++ back-end/src/main/java/admin/parkWise/administration/configs/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("register", "login", "/get", "/send").permitAll() // TODO: MUST REMOVE after finishing implementing in Flutter
+                        .requestMatchers("register", "login").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
