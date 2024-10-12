e8cc1b61ad42510486cab52baa4ba273e4cceab8
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-10-12 15:53:53 +0300

    updated sandbox for bucket storage

--- back-end/src/main/java/admin/parkWise/administration/services/AwsStorageService.java
+++ back-end/src/main/java/admin/parkWise/administration/services/AwsStorageService.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Paths;
 
 @Service
 public class AwsStorageService {
@@ -33,12 +36,11 @@ public class AwsStorageService {
         
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key(fileName).build();
+                .key(fileName)
+                .build();
 
         try {
-            PutObjectResponse resp = s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
-
-//            resp.
+            PutObjectResponse resp = s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));\
 
             // VEZI CA AICI ESTE CHETIA ASTA DE ETAG
             // SA VERIFICI DACA E ACCEIASI CU AIA CU CERE PUTEM ACCESA PUBLIC
