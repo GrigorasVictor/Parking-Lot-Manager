2a1505132783c1ea1845b7a8dc9d37459541e2f0
GrigorasVictor <victorandrei201112@gmail.com>
2024-10-18 22:36:13 +0300

    confilct fix

--- pythonAI/main.py
+++ pythonAI/main.py
@@ -1,8 +1,7 @@
 from scripts.tcpServer import start_tcp_server as startServer
 from scripts.ipv4Grabber import getIpv4
 
-
 local_ip = getIpv4()
 port = 9001
 
-startServer(local_ip, port) 
+startServer(local_ip, port)
