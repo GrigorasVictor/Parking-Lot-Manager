b9880898b95a210ff2f2d82aaba9b92a436e6df6
GrigorasVictor <victorandrei201112@gmail.com>
2024-11-02 21:33:30 +0200

    modified main.py

--- pythonAI/main.py
+++ pythonAI/main.py
@@ -1,7 +1,7 @@
 from scripts.tcpServer import start_tcp_server as startServer
-# from scripts.ipv4Grabber import getIpv4
+from scripts.ipv4Grabber import getIpv4
 
-# local_ip = getIpv4()
+local_ip = getIpv4()
 port = 9001
 
 
